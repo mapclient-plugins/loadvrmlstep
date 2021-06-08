dfd9fc826f49ecd024c7a990f99139e7cd4bcad3
TimmyNic <tim.nicolas@outlook.com>
2021-06-09 09:12:30 +1200

    Updated ConfigureDialog() arguments.

--- mapclientplugins/loadvrmlstep/step.py
+++ mapclientplugins/loadvrmlstep/step.py
@@ -81,7 +81,7 @@ class LoadVRMLStep(WorkflowStepMountPoint):
         then set:
             self._configured = True
         '''
-        dlg = ConfigureDialog()
+        dlg = ConfigureDialog(self._main_window)
         dlg.identifierOccursCount = self._identifierOccursCount
         dlg.setConfig(self._config)
         dlg.validate()
